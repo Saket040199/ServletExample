19055346296073f4021f9d6a45d957995bbd1356
saket <saketj420@gmail.com>
2021-06-15 00:35:39 +0530

    Validating USERNAME

--- src/main/java/com/bl/firstServlet/LoginServlet.java
+++ src/main/java/com/bl/firstServlet/LoginServlet.java
@@ -2,6 +2,8 @@ package com.bl.firstServlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,12 +15,9 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
 		description = "Login Servelet Testing",
-		urlPatterns = {"/LoginServlet" },
-		initParams = {
-				@WebInitParam(name ="user" , value = "Saket"),
-				@WebInitParam(name="password" , value="Saket@420")
-		    }
-		)
+		urlPatterns = {"/LoginServlet" }
+
+ 		)
 
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -31,16 +30,16 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	     String user = request.getParameter("user");
 	     String pwd = request.getParameter("pwd");
-	     
-	     String userID = getServletConfig().getInitParameter("user");
-	     String password = getServletConfig().getInitParameter("password");
-	     if(userID.equals(user) && password.equals(pwd)) {
+	 	 String name="^[(A-Z){1}][a-z]{3,}";
+         Pattern compile =Pattern.compile( name ); 
+		 Matcher match= compile.matcher(user);
+	     if(match.matches()) {
 	    	 request.setAttribute("user", user);
 	    	 request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
 	     }else {
 	    	 RequestDispatcher rd= getServletContext().getRequestDispatcher("/Login.html");
 	    	 PrintWriter out = response.getWriter();
-	    	 out.println("<font color =red>Either USERNAME or PASSWORD is wrong.</font>");
+	    	 out.println("<font color =red> USERNAME is wrong.</font>");
 	    	 rd.include(request, response);
 	     }
 	
